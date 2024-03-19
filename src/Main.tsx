9f8540c9ec4962e7e39d69f2a396ef90145e98e0
kaiyi <kaiyisahroslan@gmail.com>
2024-03-19 10:13:38 +0800

    first commit

---
+++ src/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Hello there!!
+    </h1>
+  );  
+}
+
+export default Main;
